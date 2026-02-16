21d211666c1b4d1b78ab9d4eb55797aa81c3830b
Matt Hosch <mhosch24@gmail.com>
2026-02-15 19:39:08 -0600

    Added godotenv

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"log"
 
+	"github.com/joho/godotenv"
 	"github.com/matthosch/social/internal/env"
 )
 
 func main() {
+	// Initialize environment variables
+	err := godotenv.Load()
+	if err != nil {
+		log.Print("Error loading .env file")
+	}
 
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/matthosch/social
 go 1.25.1
 
 require github.com/go-chi/chi/v5 v5.2.5
+
+require github.com/joho/godotenv v1.5.1 // indirect
