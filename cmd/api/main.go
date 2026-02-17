e5bdd56116bb171d088e0cb6f0d7bd45ad848443
Matt Hosch <mhosch24@gmail.com>
2026-02-16 20:29:09 -0600

    Added struct and interfaces for storage

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/joho/godotenv"
 	"github.com/matthosch/social/internal/env"
+	"github.com/matthosch/social/internal/store"
 )
 
 func main() {
@@ -18,8 +19,11 @@ func main() {
 		addr: env.GetString("ADDR", ":8080"),
 	}
 
+	store := store.NewStorage(nil)
+
 	app := &application{
 		config: cfg,
+		store:  store,
 	}
 
 	mux := app.mount()
